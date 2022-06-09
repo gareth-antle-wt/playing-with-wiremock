73e6395e48ae3bb7de0d14fae4eb337b0b30b680
Gareth Antle <garethantle@WTMBP-Gareth-Antle.local>
2022-06-09 13:40:53 -0700

    initial commit, bugs in gradle file but it builds

---
+++ app/src/androidTest/java/com/example/playingwithwiremock/ExampleInstrumentedTest.kt
@@ -0,0 +1,33 @@
+package com.example.playingwithwiremock
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.playingwithwiremock", appContext.packageName)
+    }
+
+    @Test
+    fun testButtonIsPresent() {
+        Espresso.onView(ViewMatchers.withId(R.id.button))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
---
+++ app/src/main/java/com/example/playingwithwiremock/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.playingwithwiremock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, "TEST", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
